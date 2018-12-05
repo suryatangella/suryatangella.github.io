e2d7d3970a649dc5aa4cee02b10a2f8732f1c56f
suryatangella <Surya.tangella@yahoo.com>
2018-12-05 05:41:17 +0000

    Update angular.js

--- hunt/scripts/libraries/angular.js
+++ hunt/scripts/libraries/angular.js
@@ -56,43 +56,44 @@ $(function() {
     })
 });
 
-var target_date = new Date("Dec 7, 2018 11:00:00").getTime() ; // set the countdown date
-var days, hours, minutes, seconds; // variables for time units
+<script>
+// Set the date we're counting down to
+var countDownDate = new Date("Dec 5, 2018 15:37:25").getTime();
 
-var countdown = document.getElementById("tiles"); // get tag element
+//document.write(countDownDate);
 
-getCountdown();
+// Update the count down every 1 second
+var x = setInterval(function() {
 
-setInterval(function () { getCountdown(); }, 1000);
-
-function getCountdown(){
-
-	// find the amount of "seconds" between now and target
-  var currentTime = new Date();
+    // Get todays date and time
+    var now = new Date().getTime();
+    
+    var currentTime = new Date();
 
 	var currentOffset = currentTime.getTimezoneOffset();
 
 	var ISTOffset = 330;   // IST offset UTC +5:30 
 
-	var current_date = new Date(currentTime.getTime() + (ISTOffset + currentOffset)*60000);
-
-	var seconds_left = (target_date - current_date) / 1000;
-
-	days = pad( parseInt(seconds_left / 86400) );
-	seconds_left = seconds_left % 86400;
-		 
-	hours = pad( parseInt(seconds_left / 3600) );
-	seconds_left = seconds_left % 3600;
-		  
-	minutes = pad( parseInt(seconds_left / 60) );
-	seconds = pad( parseInt( seconds_left % 60 ) );
-
-	// format countdown string + set tag value
-	countdown.innerHTML = "<span>" + days + "</span><span>" + hours + "</span><span>" + minutes + "</span><span>" + seconds + "</span>"; 
-}
-
-function pad(n) {
-	return (n < 10 ? '0' : '') + n;
-}
-
+	var ISTTime = new Date(currentTime.getTime() + (ISTOffset + currentOffset)*60000);
+    
+    // Find the distance between now and the count down date
+    var distance = countDownDate - ISTTime;
+    
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    // Output the result in an element with id="demo"
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+    
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
+</script>
 
